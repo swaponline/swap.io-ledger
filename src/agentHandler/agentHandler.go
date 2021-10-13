46afebdedcc1ddd5c48ac62d4c6069318f940eae
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-10-14 03:59:31 +0800

    add agent listen

--- src/agentHandler/agentHandler.go
+++ src/agentHandler/agentHandler.go
@@ -1,86 +1,99 @@
 package agentHandler
 
 import (
-	"encoding/json"
-	"fmt"
-	socketio_client "github.com/zhouhui8915/go-socket.io-client"
 	"log"
+    "net/url"
+    "encoding/json"
+
+    "github.com/gorilla/websocket"
 )
 
 type AgentHandler struct {
 	network     string
 	baseUrl     string
 	apiKey      string
-	txs         chan <- *Transaction
-	txIsReceive <- chan struct{}
-	isSync <-chan struct{}
+	txs         chan<- *Transaction
+	txIsReceive <-chan struct{}
+	isSync      <-chan struct{}
 }
 
 type Config struct {
 	Network string
 	BaseUrl string
-	ApiKey string
+	ApiKey  string
 }
 
-func initialiseAgentHandler(config Config) *AgentHandler {
+func InitialiseAgentHandler(config Config) *AgentHandler {
 	handler := AgentHandler{
-		network: config.Network,
-		baseUrl: config.BaseUrl,
-		apiKey:  config.ApiKey,
-		txs: make(chan <- *Transaction),
-		txIsReceive: make(<- chan struct{}),
-		isSync: make(<-chan struct{}),
+		network:     config.Network,
+		baseUrl:     config.BaseUrl,
+		apiKey:      config.ApiKey,
+		txs:         make(chan<- *Transaction),
+		txIsReceive: make(<-chan struct{}),
+		isSync:      make(<-chan struct{}),
 	}
+    defer close(handler.txs)
 
-	go handler.run()
+    go func() {
+        for {
+            handler.run()
+        }
+    }()
 
 	return &handler
 }
 
 func (a *AgentHandler) run() {
-	opts := &socketio_client.Options{
-		Transport: "websocket",
-		Query:     make(map[string]string),
-	}
-	opts.Query["token"] = a.apiKey
-	uri := fmt.Sprintf("%v/socket.io/", a.baseUrl)
+    u := url.URL{Scheme: "ws", Host: a.baseUrl, Path: "/ws"}
+    c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+    if err != nil {
+        log.Panicln(err)
+    }
+    defer c.Close()
+    log.Printf(
+        "connected agent(network:%v|baseUrl:%v)",
+        a.network,
+        a.baseUrl,
+    )
 
-	client, err := socketio_client.NewClient(uri, opts)
-	if err != nil {
-		log.Panicf("NewClient error:%v", err)
-	}
+    //c.SetPingHandler(func(data string) error {
+    //    return nil
+    //})
 
-	err = client.On("error", func() {
-		log.Println("on error")
-	})
-	if err != nil {
-		log.Panicln(err)
-	}
+    done := make(chan struct{})
+    defer close(done)
 
-	err = client.On("connection", func() {
-		log.Println("on connect")
-	})
-	if err != nil {
-		log.Panicln(err)
-	}
+    go func() {
+        loop: for {
+            select {
+            case <- a.txIsReceive: {
+                err := c.WriteMessage(websocket.TextMessage, []byte{})
+                if err != nil {
+                    log.Println("ERROR:", err)
+                }
+            }
+            default: {
+                break loop
+            }
+            }
+        }
+    }()
 
-	err = client.On("newTransaction", func(txStr string) {
-		var tx *Transaction
-		if err = json.Unmarshal([]byte(txStr), &tx); err != nil {
-			log.Println("ERROR", err, txStr)
-		}
-		a.txs <- tx
-		<- a.txIsReceive
-		log.Printf("%v| tx receive :%v", a.network, tx.Hash)
-	})
-	if err != nil {
-		log.Panicln(err)
-	}
+    loop: for {
+        _, msg, err := c.ReadMessage()
+        if err != nil {
+            log.Println("ERROR:", err)
+        }
 
-	err = client.On("disconnection", func() {
-		log.Println("on disconnect")
-	})
-	if err != nil {
-		log.Panicln(err)
-	}
+        var tx *Transaction
+        if err := json.Unmarshal(msg,&tx); err != nil {
+            log.Println("ERROR:", err)
+            continue
+        }
+
+        select {
+        case a.txs <- tx: continue loop
+        default: break loop
+        }
+    }
 }
